5abb714159363c808c36437791e5381ae3dab05c
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-05-14 17:09:53 +0200

    aggiunta effettiva funzionalità budgetMode price <= 30 funzionante

--- src/components/CardsWrapper.jsx
+++ src/components/CardsWrapper.jsx
@@ -3,19 +3,34 @@ import { useEffect } from "react";
 import { Link } from "react-router";
 import { useNavigate } from "react-router";
 import Card from "./Card";
+import { BudgetContext } from "../contexts/BudgetContext";
+import { useContext } from "react";
 
 function Cards_wrapper({ productList }) {
-    const navigate = useNavigate()
+
+    const { budgetMode } = useContext(BudgetContext);
+    const navigate = useNavigate();
+    const budgetList = productList.filter(current => {
+        return current.price <= 30;
+    });
 
     return (
         <div className="container">
             <div className="cards-wrapper row">
-                {
-                productList.map(current => {
-                    return <div key={current.id} onClick={() => {navigate(`/card/${current.id}`)}} className="col-3 col-lg-4 text-decoration-none text-dark">
+                {budgetMode ? 
+                (
+                    budgetList.map(current => {
+                        return <div key={current.id} onClick={() => { navigate(`/card/${current.id}`) }} className="col-3 col-lg-4 text-decoration-none text-dark">
+                            <Card singleProduct={current} />
+                        </div>;
+                    })
+                ) :
+                (productList.map(current => {
+                    return <div key={current.id} onClick={() => { navigate(`/card/${current.id}`) }} className="col-3 col-lg-4 text-decoration-none text-dark">
                         <Card singleProduct={current} />
                     </div>;
-                })
+                }))
+
                 }
             </div>
         </div>
